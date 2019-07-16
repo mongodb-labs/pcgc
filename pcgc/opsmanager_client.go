0da812f1268202847c6e93770d9e750c819047df
Mihai Bojin <mihai.bojin@mongodb.com>
2019-07-16 22:06:27 +0100

    Fixed user response

--- pcgc/opsmanager_client.go
+++ pcgc/opsmanager_client.go
@@ -13,28 +13,38 @@ type opsManagerAPI struct {
 
 // OpsManagerUser request object which identifies a user
 type OpsManagerUser struct {
-	Username     string `json:"username"`
-	Password     string `json:"password,omitempty"`
-	FirstName    string `json:"firstName"`
-	LastName     string `json:"lastName"`
+	Username string `json:"username"`
+	Password string `json:"password,omitempty"`
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
 	EmailAddress string `json:"emailAddress,omitempty"`
 }
 
 // OpsManagerUserRole denotes a single user role
-type OpsManagerUserRole struct {
-	GroupID  string `json:"groupId,omitempty"`
-	OrgID    string `json:"orgId,omitempty"`
-	RoleName string `json:"roleName,omitempty"`
+type UserRole struct {
+	RoleName string `json:"roleName"`
+	GroupID string `json:"groupId,omitempty"`
+	OrgID string `json:"orgId,omitempty"`
+}
+
+// OpsManagerUserLink denotes a single user link
+type UserLink struct {
+	HREF string `json:"groupId"`
+	OrgID string `json:"orgId"`
+}
+
+type UserResponse struct {
+	OpsManagerUser
+
+	ID string `json:"id"`
+	Links []UserLink  `json:"links,omitempty"`
+	Roles []UserRole  `json:"roles,omitempty"`
 }
 
 // CreateFirstUserResponse API response for the CreateFirstUser() call
 type CreateFirstUserResponse struct {
-	OpsManagerUser
-
-	ID           string               `json:"id"`
-	Links        []interface{}        `json:"links,omitempty"`
-	MobileNumber string               `json:"mobileNumber,omitempty"`
-	Roles        []OpsManagerUserRole `json:"roles,omitempty"`
+	ApiKey string `json:"apiKey"`
+	User UserResponse `json:"user"`
 }
 
 // OpsManagerClient defines the API actions implemented in this client
@@ -46,10 +56,11 @@ type OpsManagerClient interface {
 
 // NewOpsManagerClient builds a new API client for connecting to Ops Manager
 func NewOpsManagerClient(resolver URLResolver) OpsManagerClient {
-	return opsManagerAPI{BasicHTTPOperation: NewClient(), resolver: resolver}
+	return opsManagerAPI{BasicHTTPOperation: NewClient(), resolver:resolver}
 }
 
-// CreateFirstUser registers the first ever Ops Manager user
+
+// CreateFirstUser registers the first ever Ops Manager user (global owner)
 // https://docs.opsmanager.mongodb.com/master/reference/api/user-create-first/
 func (api opsManagerAPI) CreateFirstUser(user OpsManagerUser, whitelistIP string) (CreateFirstUserResponse, error) {
 	var result CreateFirstUserResponse
